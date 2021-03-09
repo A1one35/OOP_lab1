622dcbb84acf2b0a0ac457087e5280307dae6b5a
A1one35 <58321685+A1one35@users.noreply.github.com>
2021-03-09 20:18:17 +0200

    first

---
+++ lab1/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Coral;
+            this.label1.Text = "Початок роботи";
+            this.label1.Size = new System.Drawing.Size(100, 50);
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            label1.Visible = true;
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            label1.Visible = false;
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
